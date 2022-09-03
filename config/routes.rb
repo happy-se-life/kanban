d3aaa7cde93c300dce3a9d76590a87cecd80bc5f
Kohei Nomura <kohei_nom@yahoo.co.jp>
2022-09-03 17:25:39 +0900

    Cancel the change in #38. #42

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   resources :kanban
   post 'update_status', to: 'issue#update_status'
+  post 'get_journal', to: 'journal#get_journal'
   post 'put_journal', to: 'journal#put_journal'
 end
